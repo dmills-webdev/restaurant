9abafdb6e996b861701d1578b948997c98028700
crongle <gasmazk73@gmail.com>
2020-03-18 14:59:52 +0000

    Add basic stylesheet

--- src/landing.js
+++ src/landing.js
@@ -1,7 +1,20 @@
 const landing = () => {
   const container = document.getElementById("container");
+
+  const subContainer = document.createElement("div");
+  subContainer.classList.add("sub-container__landing");
+
   const tagline = document.createElement("div");
-  tagline.textContent = "We make the best dumplings in town, guaranteed!";
-  container.appendChild(tagline);
+  tagline.textContent = "the best dumplings you'll ever taste.";
+  tagline.classList.add("img1-tagline")
+  subContainer.appendChild(tagline);
+
+  const img1 = document.createElement("img");
+  img1.src = "../src/img/chinese-food-1.jpeg";
+  img1.classList.add("img1");
+  subContainer.appendChild(img1);
+
+  container.appendChild(subContainer);
+
 }
 export default landing;
